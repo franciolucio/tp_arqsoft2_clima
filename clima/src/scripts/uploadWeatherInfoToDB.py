fade6f79faeb9491217e2a29cb082243190b5acd
Emiliano Mancuso <emiliano07.mp@gmail.com>
2022-06-17 15:03:31 +0200

    Change job

--- clima/src/scripts/uploadWeatherInfoToDB.py
+++ clima/src/scripts/uploadWeatherInfoToDB.py
@@ -5,7 +5,7 @@ import time
 def job():
     uploadWeatherInfoToDB()
             
-schedule.every().hour.at(":00:00").do(job)
+schedule.every(5).minutes.at(":00").do(job)
 
 while True:
     schedule.run_pending()
